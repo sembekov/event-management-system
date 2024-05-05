5888ceeae82ab9934c7130a1fbbaa66434051259
sembekov <76952248+sembekov@users.noreply.github.com>
2024-05-05 16:46:18 +0500

    Add repository annotation

--- src/main/java/org/example/eventmanagementsystem/repositories/CityRepository.java
+++ src/main/java/org/example/eventmanagementsystem/repositories/CityRepository.java
@@ -2,6 +2,8 @@ package org.example.eventmanagementsystem.repositories;
 
 import org.example.eventmanagementsystem.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 }
--- src/main/java/org/example/eventmanagementsystem/repositories/TicketRepository.java
+++ src/main/java/org/example/eventmanagementsystem/repositories/TicketRepository.java
@@ -2,6 +2,8 @@ package org.example.eventmanagementsystem.repositories;
 
 import org.example.eventmanagementsystem.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
